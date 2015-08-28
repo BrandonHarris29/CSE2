fdd988a0c529741d36a31f5c01597020f2128842
brandonharris29 <brandon.harris29@gmail.com>
2015-08-28 16:02:53 +0000

    Updated Hello World

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,21 @@
+//Brandon Harris
+//Lab 01
+// 8/28/15
+
+//class definition
+public class HelloWorld{
+    //main method
+    
+    public static void main(String[] args){
+        //print out the statement
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("#                      #");
+        System.out.println("#  Hello World 2014!   #");
+        System.out.println("#                      #");
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("test");
+    }
+    
+}
