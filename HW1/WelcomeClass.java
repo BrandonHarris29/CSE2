c986c45f304e6c9c08c9bb5ef2f32a0350ba65f5
brandonharris29 <brandon.harris29@gmail.com>
2015-09-01 04:17:20 +0000

    updated HW1

---
+++ HW1/WelcomeClass.java
@@ -0,0 +1,23 @@
+//Brandon Harris
+//HW 01
+// 8/31/15
+
+//class definition
+public class WelcomeClass{
+    //main method
+    
+    public static void main(String[] args){
+        //print out the statement
+        System.out.println("-----------");
+        System.out.println("| WELCOME |");
+        System.out.println("-----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+        System.out.println("<-B--E--H--2--1--7->");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v");
+        System.out.println("My name is Brandon Harris. I am a Junior Electrical Engineering and Physics Double major from Watchung, New Jersey ");
+        
+    }
+    
+}
